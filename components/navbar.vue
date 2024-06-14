9f186ee00c8b41b5acb7b54b81f3652b325b6e98
Tássio Montenegro <109485884+tm0nt@users.noreply.github.com>
2024-06-14 16:28:00 -0300

    Up

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
       <v-app-bar
-        color="rgb(0,0,0,0)"
+        color="background"
       >
         <v-app-bar-title>
             <v-img src="https://i.imgur.com/rA59f9X.png" width="180" class="mx-auto"></v-img>
